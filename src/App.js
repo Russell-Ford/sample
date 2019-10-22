eecc525a09eecbc8a0e99f43c393fed104cb0e34
Russell <russellford96@gmail.com>
2019-10-22 19:34:25 -0400

    removed unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomTables from './CustomTables.js';
-import ExampleTables from './ExampleTables.js';
 
 function App() {
   return (
